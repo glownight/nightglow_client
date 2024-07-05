5d1db39d6c4839d4a61b9842dbcd2675504f0208
glownight <885204655@qq.com>
2024-07-06 02:08:14 +0800

    2024年7月6日
    进度：第13课 14:25/16:57
    
    备注：2个仓库一堆bug
    解决办法：
    1.停止写代码
    2.有时间就把写过的代码用ai翻译一下

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React,{useState} from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+
+// const Login = () => {
+//   return (
+//     <div className='auth'>
+//       <h1>Login</h1>
+//       <form action="">
+//         <input required type="text" placeholder='username' />
+//         <input required type="password" placeholder='password' />
+//         <button>Login</button>
+//         <span>没有账户？<Link to="/register">注册</Link></span>
+//       </form>
+//     </div>
+//   )
+// }
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: ''
+  })
+  const [error, serError] = useState(null)
+
+  const navigate = useNavigate()
+  const handChange = e => {
+    setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }))
+
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      const res = await axios.post("/auth/login", inputs)
+      console.log(res);
+      navigate('/')
+
+    } catch (error) {
+      serError(error.response.data)
+
+    }
+  }
   return (
     <div className='auth'>
-      <h1>Login</h1>
+      <h1>Register</h1>
       <form action="">
-        <input required type="text" placeholder='username' />
-        <input required type="password" placeholder='password' />
-        <button>Login</button>
-        <span>没有账户？<Link to="/register">注册</Link></span>
+        <input required type="text" placeholder='username' name='username' onChange={handChange} />
+        <input required type="password" placeholder='password' name='password' onChange={handChange} />
+        <button onClick={handleSubmit}>Register</button>
+        {error && <p> {error}</p>}
+        <span>没有账户？<Link to="/login">注册</Link></span>
       </form>
     </div>
   )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Register = () => {
   const [inputs, setInputs] = useState({
@@ -8,10 +8,15 @@ const Register = () => {
     email: '',
     password: ''
   })
+  const [error,serError] = useState(null)
+
+  const navigate = useNavigate()
   const handChange = e => {
     setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }))
 
   }
+
+
 
   // console.log('====================================');
   // console.log(inputs);
@@ -22,9 +27,11 @@ const Register = () => {
       // const res = axios.post("http://localhost:5173/auth/register", inputs)
       const res = await axios.post("/auth/register", inputs)
       console.log(res);
+      navigate('/login')
 
     } catch (error) {
-      console.log('handleSubmit 错误',error);
+      // console.log('handleSubmit 错误',error);
+      serError(error.response.data)
 
     }
   }
@@ -36,6 +43,7 @@ const Register = () => {
         <input required type="email" placeholder='email' name='email' onChange={handChange} />
         <input required type="password" placeholder='password' name='password' onChange={handChange} />
         <button onClick={handleSubmit}>Register</button>
+        {error && <p> {error}</p>}
         <span>已有账户？<Link to="/login">登陆</Link></span>
       </form>
     </div>
