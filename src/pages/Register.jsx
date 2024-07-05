124bdf238671ea2209e2a6b73bab312dcfeb37bc
glownight <885204655@qq.com>
2024-07-06 01:28:56 +0800

    0706 进度：写到了第13课 11:10 共16:57
    遇到的问题： axios.post("/auth/register", inputs) 请求不到数据

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: '',
+    password: ''
+  })
+  const handChange = e => {
+    setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }))
+
+  }
+
+  // console.log('====================================');
+  // console.log(inputs);
+  // console.log('====================================');
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      // const res = axios.post("http://localhost:5173/auth/register", inputs)
+      const res = await axios.post("/auth/register", inputs)
+      console.log(res);
+
+    } catch (error) {
+      console.log('handleSubmit 错误',error);
+
+    }
+  }
   return (
     <div className='auth'>
       <h1>Register</h1>
       <form action="">
-        <input required type="text" placeholder='username' />
-        <input required type="email" placeholder='email' />
-        <input required type="password" placeholder='password' />
-        <button>Register</button>
+        <input required type="text" placeholder='username' name='username' onChange={handChange} />
+        <input required type="email" placeholder='email' name='email' onChange={handChange} />
+        <input required type="password" placeholder='password' name='password' onChange={handChange} />
+        <button onClick={handleSubmit}>Register</button>
         <span>已有账户？<Link to="/login">登陆</Link></span>
       </form>
     </div>
